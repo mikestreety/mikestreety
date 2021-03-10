11e4d18af2081736fe4f8c89c79949803d98bbfe
Mike Street <mikestreety@gmail.com>
2021-03-10 19:49:15 +0000

    More stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 mix
 	.sass('build/css/screen.scss', 'html/assets/css/style.css')
 	.copyDirectory('build/fonts', 'html/assets/fonts')
+	.copyDirectory('build/img', 'html/assets/img')
 	.js('build/js/app.js', 'html/assets/js/app.js')
 	.setPublicPath('html/assets');
 
