f1690995aad863e0b3f650730dab3d80105d0fe1
Mike Street <mike@liquidlight.co.uk>
2021-07-20 09:50:54 +0100

    Bug: Fix style location

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix
-	.sass('build/css/screen.scss', 'html/assets/css/style.css')
+	.sass('build/css/screen.scss', 'css/style.css')
 	.copyDirectory('build/fonts', 'html/assets/fonts')
 	.copyDirectory('build/img', 'html/assets/img')
 	.setPublicPath('html/assets');
