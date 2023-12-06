2325344d46071f023119542577beb319435c3d86
Mike Street <mikestreety@gmail.com>
2023-12-06 12:23:22 +0000

    fix(webpack): Add unrot js to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const mix = require('laravel-mix');
 mix
 	.sass('build/css/screen.scss', 'css/style.css')
 	.js('build/js/charts.js', 'js/charts.js')
+	.js('build/js/unrot-link.js', 'js/unrot-link.js')
 	.copyDirectory('build/fonts', 'html/assets/fonts')
 	.copyDirectory('build/img', 'html/assets/img')
 	.setPublicPath('html/assets');
